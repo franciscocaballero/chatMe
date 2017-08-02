2e04b3b0de30be3fa3313c95eec10072f87fcfcb
franciscocaballero <caballerofrancisco22@yahoo.com>
2017-08-01 21:41:27 -0400

    initial setup completed

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name chatMeApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the chatMeApp
+ */
+angular.module('chatMeApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name chatMeApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the chatMeApp
+ */
+angular.module('chatMeApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
